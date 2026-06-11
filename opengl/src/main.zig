8f153876439101f14fcf1fe203bd793fe76d5d61
Dovonun <31408384+Dovonun@users.noreply.github.com>
2026-06-11 23:12:35 +0200

    complete opengl hello window in zig

--- opengl/src/main.zig
+++ opengl/src/main.zig
@@ -3,12 +3,12 @@ const std = @import("std");
 const c = @import("c");
 
 const Io = std.Io;
-
-// fn gladGlfwGetProcAddress(name: [*c]const u8) callconv(.C) ?*anyopaque {
-//     const proc = c.glfwGetProcAddress(name) orelse return null;
-//     return @ptrCast(proc);
+// TODO: This callback needs to be passed to the C lib. So how do I make this follow the c abi?
+// extern fn framebuffer_size_callback(window c.GLFWwindow, width c_int, height c_int) {
+//   glViewport(0, 0, width, height);
 // }
-fn gladGlfwGetProcAddress(name: [*c]const u8) callconv(.C) ?*anyopaque {
+
+fn gladGlfwGetProcAddress(name: [*c]const u8) callconv(.c) ?*anyopaque {
     const proc = c.glfwGetProcAddress(name) orelse return null;
     return @constCast(@ptrCast(proc));
 }
@@ -19,18 +19,25 @@ pub fn main() !void {
     if (c.glfwInit() == 0) return error.GlfwInitFailed;
     defer c.glfwTerminate();
 
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
+    c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+
     const window = c.glfwCreateWindow(800, 600, "Zig + GLFW", null, null) orelse return error.GlfwCreateWindowFailed;
-    defer c.glfwDestroyWindow();
+    defer c.glfwDestroyWindow(window);
 
     c.glfwMakeContextCurrent(window);
-    // if (c.gladLoadGLLoader(@ptrCast(c.glfwGetProcAddress)) == 0) {
-    //     return error.GladLoadFailed;
-    // }
     if (c.gladLoadGLLoader(gladGlfwGetProcAddress) == 0) {
         return error.GladLoadFailed;
     }
+    // c.glfwSetFramebufferSizeCallback( window,);
+    c.glfwSwapInterval(1);
+    var framebuffer_width: c_int, var framebuffer_height: c_int = .{ 0, 0 };
+    c.glfwGetFramebufferSize(window, &framebuffer_width, &framebuffer_height);
+    c.glViewport(0, 0, framebuffer_width, framebuffer_height);
 
     while (c.glfwWindowShouldClose(window) == 0) {
+        if (c.glfwGetKey(window, c.GLFW_KEY_ESCAPE) == c.GLFW_PRESS) c.glfwSetWindowShouldClose(window, 1);
         c.glClearColor(0.1, 0.2, 0.3, 1.0);
         c.glClear(c.GL_COLOR_BUFFER_BIT);
 
